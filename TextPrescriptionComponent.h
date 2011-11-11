d993f8e7db886b80516ef40870f53145a9df9dc6
misaelpc <misaelpcgm@gmail.com>
2011-11-11 12:51:11 -0600

    Layers added to prescriptionView
    New Class added for control of the components Layers

---
+++ TextPrescriptionComponent.h
@@ -0,0 +1,18 @@
+//
+//  TextPrescriptionComponent.h
+//  pocGeometry
+//
+//  Created by Misael Pérez Chamorro on 11/10/11.
+//  Copyright (c) 2011 Emlink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TextPrescriptionComponent : NSObject
+{
+	Boolean isEditionEnabled;
+}
+
+@property (nonatomic,assign) Boolean isEditionEnabled;
+
+@end
---
+++ TextPrescriptionComponent.m
@@ -0,0 +1,14 @@
+//
+//  TextPrescriptionComponent.m
+//  pocGeometry
+//
+//  Created by Misael Pérez Chamorro on 11/10/11.
+//  Copyright (c) 2011 Emlink. All rights reserved.
+//
+
+#import "TextPrescriptionComponent.h"
+
+@implementation TextPrescriptionComponent
+
+@synthesize isEditionEnabled;
+@end
